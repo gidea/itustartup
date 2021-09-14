28b289264d93e57fa16a20982dd0d81cf9ad2879
Vlad Gidea <vlad.gidea@gmail.com>
2021-09-14 12:15:15 +0200

    cofounders link fixed

--- src/components/hero.js
+++ src/components/hero.js
@@ -39,7 +39,7 @@ class Hero extends React.Component {
                         Join the incubator
                       </a>
                       <a
-                        href="/matchmaking"
+                        href="/cofounders"
                         className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
                       >
                         Find a cofounder
