fd75e8b40cc4a1a4f85578fe316ecbbc4257e9fa
Vlad Gidea <vlad.gidea@gmail.com>
2021-09-14 13:16:43 +0200

    testimonial update

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -61,9 +61,7 @@ class Testimonials extends React.Component {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-              The Startup Programme was instrumental in getting our company from an idea to an early business:
-              very useful legal counsel, capital injections and overall advice. ITU-BD is a great advantage for students’ startups
-              as it brings experience which is critical to creating a profitable company.
+              The ITU Startup programme has helped us to move from an idea stage to having a company with a product and a great team. Vlad Gidea, the incubator manager, helped us refine the idea we had and guided us to target the right customer segment, while also helping us to design our prototype.
               </p>
             </div>
             <footer className="mt-8">
@@ -76,8 +74,8 @@ class Testimonials extends React.Component {
                   />
                 </div>
                 <div className="ml-4">
-                  <div className="text-base font-medium text-white">Samy Tessier</div>
-                  <div className="text-base font-medium text-indigo-200">CEO, Clockwork Trading ApS</div>
+                  <div className="text-base font-medium text-white">Victor Brorson</div>
+                  <div className="text-base font-medium text-indigo-200">CEO, useWhere.com</div>
                 </div>
               </div>
             </footer>
